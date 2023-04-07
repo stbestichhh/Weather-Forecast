d534e60c773445fa7cc4a7e1ccbe243105e4b7f5
Kirill Yaymchuk <stbestich@gmail.com>
2023-04-07 16:05:51 +0300

    ApiService method
    This method creates new HTTP client and gets weather by api, then deserializes json file

--- WeatherApp/Services/ApiService.cs
+++ WeatherApp/Services/ApiService.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using Newtonsoft.Json;
+using WeatherApp.Models;
+
 namespace WeatherApp.Services
 {
     public class ApiService
     {
-        public ApiService()
+        public async Task<Root> GetWeather(double latitude, double longitude)
         {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(string.Format("http://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&appid=98cf0673e88ccd55a86fda0cfe7e0a80", latitude, longitude));
+            return JsonConvert.DeserializeObject<Root>(response);
         }
     }
 }
