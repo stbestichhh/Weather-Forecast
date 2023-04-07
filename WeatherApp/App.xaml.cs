cde97c2b3218d0533b5b8da4589f16271d7b9b51
Kirill Yaymchuk <stbestich@gmail.com>
2023-04-07 20:48:55 +0300

    Creted new page and Aded version tracking

--- WeatherApp/App.xaml.cs
+++ WeatherApp/App.xaml.cs
@@ -5,6 +5,11 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
+        VersionTracking.Track();
+        if(VersionTracking.IsFirstLaunchEver)
+        {
+            MainPage = new FirstLaunchPage();
+        }
 
         MainPage = new WeatherPage();
     }
