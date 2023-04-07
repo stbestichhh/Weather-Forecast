08a20023501fbcc077066e1f820955a2da7bf401
Kirill Yaymchuk <stbestich@gmail.com>
2023-04-07 17:23:42 +0300

    Converted temperature units to celsium

--- WeatherApp/Models/Root.cs
+++ WeatherApp/Models/Root.cs
@@ -41,6 +41,7 @@ namespace WeatherApp.Models
     public class Main
     {
         public double temp { get; set; }
+        public double convertedTemp => Math.Round(temp);
         public double feels_like { get; set; }
         public double temp_min { get; set; }
         public double temp_max { get; set; }
