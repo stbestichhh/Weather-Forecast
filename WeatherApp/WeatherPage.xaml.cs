59429b36e774590d3ca7c3469326a5ad46fb2321
Kirill Yaymchuk <stbestich@gmail.com>
2023-04-07 19:13:37 +0300

    Getting weather by current location buttton
    added most of all OnAppearing() method logic to another method which is called by OnAppearing() and OnLocationButtonClicked()

--- WeatherApp/WeatherPage.xaml.cs
+++ WeatherApp/WeatherPage.xaml.cs
@@ -18,9 +18,27 @@ public partial class WeatherPage : ContentPage
     {
         base.OnAppearing();
         await GetUsersLocation();
+        await GetWeatherByLocationButton(latitude, longitude);
+    }
+
+    public async Task GetUsersLocation()
+    {
+        var userLocation = await Geolocation.GetLocationAsync();
+        latitude = userLocation.Latitude;
+        longitude = userLocation.Longitude;
+    }
+
+    private async void OnLocationButtonClicked(object sender, EventArgs e)
+    {
+        await GetUsersLocation();
+        await GetWeatherByLocationButton(latitude, longitude);
+    }
+
+    public async Task GetWeatherByLocationButton(double latitute, double longitude)
+    {
         var getWeather = await ApiService.GetWeather(latitude, longitude); //latitude and longitude
 
-        foreach(var weatherOption in getWeather.list)
+        foreach (var weatherOption in getWeather.list)
         {
             WeatherList.Add(weatherOption);
         }
@@ -32,12 +50,5 @@ public partial class WeatherPage : ContentPage
         hydrometerValueLabel.Text = getWeather.list[0].main.humidity + " %"; //humidity - (ru)влажность
         windSpeedLabel.Text = getWeather.list[0].wind.speedInMeters + " m/s"; //wind speed
         weatherConditionsImage.Source = getWeather.list[0].weather[0].weatherImage; //weather image
-    }
-
-    public async Task GetUsersLocation()
-    {
-        var userLocation = await Geolocation.GetLocationAsync();
-        latitude = userLocation.Latitude;
-        longitude = userLocation.Longitude;
     }
 }
