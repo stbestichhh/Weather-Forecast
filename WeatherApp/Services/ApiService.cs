1765fc3e38380622380d0ad0bd3d5bdf6688ae6a
Kirill Yaymchuk <stbestich@gmail.com>
2023-04-07 16:09:23 +0300

    Api services Class new method
    Method GetWeatherByCity() creates new HTTP client object and gets weather data by city name using api

--- WeatherApp/Services/ApiService.cs
+++ WeatherApp/Services/ApiService.cs
@@ -4,12 +4,19 @@ using WeatherApp.Models;
 
 namespace WeatherApp.Services
 {
-    public class ApiService
+    public static class ApiService
     {
-        public async Task<Root> GetWeather(double latitude, double longitude)
+        public static async Task<Root> GetWeather(double latitude, double longitude)
         {
             var httpClient = new HttpClient();
-            var response = await httpClient.GetStringAsync(string.Format("http://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&appid=98cf0673e88ccd55a86fda0cfe7e0a80", latitude, longitude));
+            var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&appid=98cf0673e88ccd55a86fda0cfe7e0a80", latitude, longitude));
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+
+        public static async Task<Root> GetWeatherByCity(string city)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(string.Format("http://api.openweathermap.org/data/2.5/forecast?q={0}&appid=98cf0673e88ccd55a86fda0cfe7e0a80", city));
             return JsonConvert.DeserializeObject<Root>(response);
         }
     }
