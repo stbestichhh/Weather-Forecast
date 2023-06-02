93dbf86befac6a41255fae263289c2b6f041d1e2
Kirill Yaymchuk <stbestich@gmail.com>
2023-06-02 17:27:19 +0300

    Database constant
    SQlite flags, databesa file name and directory

---
+++ WeatherApp/Models/Constants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace WeatherApp.Models
+{
+    public class Constants
+    {
+        public const string DatabaseFilename = "SearchHistoryDatabase.db3";
+
+        public const SQLite.SQLiteOpenFlags flags =
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            SQLite.SQLiteOpenFlags.Create |
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, DatabaseFilename);
+            }
+        }
+    }
+}
+
