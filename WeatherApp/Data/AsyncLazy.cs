5877db6fd7426926a213191c7da226b150f658c7
Kirill Yaymchuk <stbestich@gmail.com>
2023-06-02 17:22:33 +0300

    Create AsyncLazy.cs

---
+++ WeatherApp/Data/AsyncLazy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace WeatherApp.Data
+{
+    public class AsyncLazy<T>
+    {
+        readonly Lazy<Task<T>> instance;
+
+        public AsyncLazy(Func<T> factory)
+        {
+            instance = new Lazy<Task<T>>(() => Task.Run(factory));
+        }
+
+        public TaskAwaiter<T> GetAwaiter()
+        {
+            return instance.Value.GetAwaiter();
+        }
+
+        public void Start()
+        {
+            var unused = instance.Value;
+        }
+    }
+}
+
