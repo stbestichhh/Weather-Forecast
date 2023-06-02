527e88fb2a791995f56b4cbf53a43c59df8a1612
Kirill Yaymchuk <stbestich@gmail.com>
2023-06-02 17:32:25 +0300

    Creating database table

---
+++ WeatherApp/Data/SearchHistoryDatabase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SQLite;
+using WeatherApp.Models;
+
+namespace WeatherApp.Data
+{
+    public class SearchHistoryDatabase
+    {
+        static SQLiteAsyncConnection Database;
+
+        public static readonly AsyncLazy<SearchHistoryDatabase> Instance = new AsyncLazy<SearchHistoryDatabase>(async () =>
+        {
+            var instance = new SearchHistoryDatabase();
+            try
+            {
+                CreateTableResult result = await Database.CreateTableAsync<SearchHistoryDatabase>();
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            return instance;
+        });
+    }
+}
+
