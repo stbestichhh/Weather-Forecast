0a79df4c9a2af11aff9f6d4c43460ae8ee1a2a65
Kirill Yaymchuk <stbestich@gmail.com>
2023-10-01 19:25:18 +0300

    Swipe handler

--- WeatherApp/WeatherForecastApp/Views/MainPage.xaml.cs
+++ WeatherApp/WeatherForecastApp/Views/MainPage.xaml.cs
@@ -9,5 +9,11 @@ public partial class MainPage : ContentPage
 
     void SearchButton_Clicked(System.Object sender, System.EventArgs e)
     {
+
+    }
+
+    private async void SwipeUpGesture_Swiped(System.Object sender, Microsoft.Maui.Controls.SwipedEventArgs e)
+    {
+        await Shell.Current.GoToAsync(AppRoutes.secondaryPageRoute);
     }
 }
