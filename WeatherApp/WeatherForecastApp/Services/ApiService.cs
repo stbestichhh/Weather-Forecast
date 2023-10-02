c3d351edb147ca4d053f731105689657e7439949
Kirill Yaymchuk <stbestich@gmail.com>
2023-10-02 13:29:51 +0300

    Created api service

--- WeatherApp/WeatherForecastApp/Services/ApiService.cs
+++ WeatherApp/WeatherForecastApp/Services/ApiService.cs
@@ -2,8 +2,20 @@
 
 namespace WeatherForecastApp.Services
 {
-    public class ApiService
+    public static class ApiService
     {
-        
+        public static async Task GetWeather(double latitude, double longitude)
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(string.Format($"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&appid=APIKEY"));
+            return JsonConvert.DeserializeObject(response);
+        }
+
+        public static async Task GetWeatherByCity(string city)
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(string.Format($"https://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&appid=APIKEY"));
+            return JsonConvert.DeserializeObject(response);
+        }
     }
 }
