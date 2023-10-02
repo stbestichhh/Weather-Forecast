81be1d38676ff67f4b66a150fd111506dec4ebd2
Kirill Yaymchuk <stbestich@gmail.com>
2023-10-02 13:42:50 +0300

    Finished with api service and data

--- WeatherApp/WeatherForecastApp/Services/ApiService.cs
+++ WeatherApp/WeatherForecastApp/Services/ApiService.cs
@@ -4,18 +4,18 @@ namespace WeatherForecastApp.Services
 {
     public static class ApiService
     {
-        public static async Task GetWeather(double latitude, double longitude)
+        public static async Task<Root> GetWeather(double latitude, double longitude)
         {
             HttpClient httpClient = new HttpClient();
             var response = await httpClient.GetStringAsync(string.Format($"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&appid=APIKEY"));
-            return JsonConvert.DeserializeObject(response);
+            return JsonConvert.DeserializeObject<Root>(response);
         }
 
-        public static async Task GetWeatherByCity(string city)
+        public static async Task<Root> GetWeatherByCity(string city)
         {
             HttpClient httpClient = new HttpClient();
             var response = await httpClient.GetStringAsync(string.Format($"https://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&appid=APIKEY"));
-            return JsonConvert.DeserializeObject(response);
+            return JsonConvert.DeserializeObject<Root>(response);
         }
     }
 }
